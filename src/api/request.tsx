804479dd3211a5e6ae310a212e98859f603a753b
Ferdinand Ismael <ferdinand.ismael@gmail.com>
2020-06-27 16:39:46 -0400

    update production api url

--- src/api/request.tsx
+++ src/api/request.tsx
@@ -2,7 +2,9 @@ import axios from 'axios';
 
 export const client = axios.create({
   baseURL:
-    process.env.NODE_ENV === 'development' ? 'http://localhost:5000' : '',
+    process.env.NODE_ENV === 'development'
+      ? 'http://localhost:5000'
+      : 'https://api-currency-exchange.herokuapp.com/',
 });
 
 const request = (options) => {
